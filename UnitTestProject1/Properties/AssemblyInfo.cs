506ac17b0723140636e742618fb4a3be768d9e7d
Aleksandar Puskas <apuskas@gunsh.com>
2019-08-23 16:51:30 +0200

    Import

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db53d143-1fe7-4b4b-9956-00ef611145b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Reflection;
+
+namespace UnitTestProject1
+{
+    [Obfuscation]
+    public interface ITest1
+    {
+        string Value { get; set; }
+    }
+
+    public interface ITest2
+    {
+        string Value { get; set; }
+    }
+
+    [Obfuscation]
+    public interface ITest3
+    {
+        string Value { get; set; }
+    }
+
+    internal class Test3 : ITest3
+    {
+        public string Value { get; set; }
+    }
+
+    [Obfuscation]
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestObfuscation()
+        {
+            var obfuscationAttribute = typeof(UnitTest1).GetCustomAttribute<ObfuscationAttribute>();
+#if OBFUSCAR
+            Assert.IsNull(obfuscationAttribute);
+#else
+            Assert.IsNotNull(obfuscationAttribute);
+#endif
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = Mock.Of<ITest1>();
+            test.Value = "314";
+            Assert.AreEqual("314", test.Value);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var test = Mock.Of<ITest2>();
+            test.Value = "314";
+            Assert.AreEqual("314", test.Value);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var test = Mock.Of<ITest3>();
+            test.Value = "314";
+            Assert.AreEqual("314", test.Value);
+        }
+    }
+}
